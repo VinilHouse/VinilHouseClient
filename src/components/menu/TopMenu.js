a2c5bbb3d1421182aedde04dfe42348677a5ee4f
kangyunsik <re12io@ajou.ac.kr>
2022-05-25 00:55:57 +0900

    feat: icons for top menu

--- src/components/menu/TopMenu.js
+++ src/components/menu/TopMenu.js
@@ -1,7 +1,8 @@
 import styled from '@emotion/styled'
-import { Button } from 'antd'
+import { Button, Avatar } from 'antd'
 import { useRecoilState } from 'recoil'
 import { modalLoginVisibleState } from 'src/store/states'
+import { AimOutlined, UserOutlined, ControlOutlined } from '@ant-design/icons'
 
 const TopMenu = () => {
   const [_, setIsModalLoginVisible] = useRecoilState(modalLoginVisibleState)
@@ -15,9 +16,15 @@ const TopMenu = () => {
 
   return (
     <StyledWrapper>
-      <Button type="primary" onClick={onClickHandler}>
-        Open Modal
-      </Button>
+      <div className="menu-div">
+        <UserOutlined onClick={onClickHandler} />
+      </div>
+      <div className="menu-div">
+        <AimOutlined />
+      </div>
+      <div className="menu-div">
+        <ControlOutlined />
+      </div>
     </StyledWrapper>
   )
 }
@@ -28,8 +35,17 @@ const StyledWrapper = styled.div`
   top: 10px;
   right: 10px;
   border-radius: 5px;
-  border: 1px solid #909090;
-  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.4);
-  background: #fff;
   z-index: 2;
+  .menu-div {
+    margin-bottom: 15px;
+    background-color: white;
+    border: 1px solid #909090;
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.4);
+    border-radius: 5px;
+    padding: 3px;
+    .anticon {
+      font-size: 40px;
+      color: '#999999';
+    }
+  }
 `
